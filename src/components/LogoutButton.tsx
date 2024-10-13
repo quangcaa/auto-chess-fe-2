6cf218e3d9be067760b0178527c70a91da481357
ductantb <vuductan3507@gmail.com>
2024-10-13 22:05:50 +0700

    logoutButton

---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+export const LogoutButton: React.FC = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogout = async (e) => {
+    try {
+      // Gửi yêu cầu đăng xuất đến server
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/logout`, {}, {
+        headers: {
+          'x_authorization': localStorage.getItem('accessToken') || '' 
+        }
+      });
+
+      if (res.data.success) {
+        localStorage.removeItem('accessToken');
+        navigate('/login');
+      }
+    } catch (error) {
+        setError("Please check your internet");
+        console.error(error);
+    }
+  };
+
+  return (
+    <button onClick={handleLogout}>
+      Logout
+    </button>
+  );
+};
+
+
