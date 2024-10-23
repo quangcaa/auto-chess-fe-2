fb9fc7596f6a94211c25549cb91b6a525c9799c9
ductantb <vuductan3507@gmail.com>
2024-10-23 10:37:18 +0700

    fixRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,48 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Landing } from './screens/Landing';
-import { Game } from './screens/Game';
-import { Auth } from './screens/Auth';
-import { Profile } from './screens/Profile';
-import { Setting } from './screens/Setting';
-import PrivateRoute from './components/PrivateRoute';
-import { AuthProvider } from './context/AuthContext';
-import NavBar from './components/NavBar';
-import { CreateTopic } from './components/CreateTopic';
-import { Forum } from './screens/Forum';
-import { TopicList } from './components/TopicList';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Landing } from "./screens/Landing";
+import { Game } from "./screens/Game";
+import { Auth } from "./screens/Auth";
+import { Profile } from "./screens/Profile";
+import { Setting } from "./screens/Setting";
+import PrivateRoute from "./components/PrivateRoute";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import NavBar from "./components/NavBar";
+import { CreateTopic } from "./components/CreateTopic";
+import { Forum } from "./screens/Forum";
+import { TopicList } from "./components/TopicList";
+import React from "react";
 
 function App() {
+  // const { isAuthenticated } = useAuth();
+
   return (
-    <div className='h-screen bg-main-color'>
+    <div className="h-screen bg-main-color">
       <AuthProvider>
         <BrowserRouter>
+          {/* {isAuthenticated && <NavBar />} */}
+          <NavBar />
+
           <Routes>
-            <Route path="/" element={<NavBar />}>
-              <Route path='/*' element={<Auth />} />
-              <Route element={<PrivateRoute />}>
-                <Route path="/" element={<Landing />} />
-                <Route path='/game' element={<Game />} />
-                <Route path='/my-profile' element={<Profile />} />
-                <Route path='/setting' element={<Setting />} />
-              </Route>
+            <Route path="/*" element={<Auth />} />
+
+            <Route element={<PrivateRoute />}>
+              <Route path="/" element={<Landing />} />
+              <Route path="/game" element={<Game />} />
+              <Route path="/my-profile" element={<Profile />} />
+              <Route path="/setting" element={<Setting />} />
+              <Route path="/forum" element={<Forum />} />
+              <Route path="/forum/category" element={<TopicList />} />
+              <Route
+                path="/forum/category/create-topic"
+                element={<CreateTopic />}
+              />
             </Route>
-            <Route path='/forum' element={<Forum />} />
-            <Route path='/forum/category' element={<TopicList />} />
-            <Route path='/forum/category/create-topic' element={<CreateTopic />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
-    </ div >
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
