913d3f4f979cec3e8b4ea2ac4c4af0a2493fd865
Maou <nguyenvietquang.1601@gmail.com>
2024-11-11 21:02:12 +0700

    fix socket chess

--- src/constants/game.js
+++ src/constants/game.js
@@ -1,8 +1,5 @@
 export const INIT_GAME = 'init_game'
 export const GAME_ADDED = 'game_added'
-export const MOVE = 'move'
-export const GAME_OVER = 'game_over'
-export const JOIN_GAME = 'join_game'
 export const JOIN_ROOM = 'join_room'
 export const OPPONENT_DISCONNECTED = 'opponent_disconnected'
 export const GAME_NOT_FOUND = 'game_not_found'
@@ -11,3 +8,27 @@ export const GAME_ENDED = 'game_ended'
 export const GAME_ALERT = 'game_alert'
 export const GAME_TIME = 'game_time'
 export const EXIT_GAME = 'exit_game'
+
+export const CREATE_GAME = 'create_game'
+export const JOIN_GAME = 'join_game'
+export const LEAVE_GAME = 'leave_game'
+
+export const GAME_CREATED = 'game_created'
+
+
+export const START_GAME = 'start_game'
+export const MOVE = 'move'
+export const GAME_OVER = 'game_over'
+export const RESIGN = 'resign'
+export const DRAW_OFFER = 'draw_offer'
+export const DRAW_RESPONSE = 'draw_response'
+
+export const SEND_MESSAGE = 'send_message'
+export const RECEIVE_MESSAGE = 'receive_message'
+export const NOTIFICATION_IN_GAME = 'notification_in_game'
+
+export const UPDATE_PLAYER_STATUS = 'update_player_status' 
+export const GET_ONLINE_PLAYERS = 'get_online_players'
+
+export const INVITE_PLAYER = 'invite_player'
+export const INVITE_RESPONSE = 'invite_response'
--- src/screens/Game.jsx
+++ src/screens/Game.jsx
@@ -1,96 +1,167 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import { useAuth } from "../contexts/AuthContext";
 import toast from "react-hot-toast";
+import {
+  CREATE_GAME,
+  GAME_OVER,
+  JOIN_GAME,
+  LEAVE_GAME,
+  MOVE,
+  START_GAME,
+} from "@/constants/game";
+import { Button } from "@/components/ui/button";
 
 export const Game = () => {
-  const [game] = useState(new Chess());
-  const [position, setPosition] = useState('start')
+  const [game, setGame] = useState(new Chess());
   const [roomId, setRoomId] = useState("");
   const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
   const [players, setPlayers] = useState({ white: null, black: null });
+  const [gameResult, setGameResult] = useState(null);
 
   const { socket } = useAuth();
 
   useEffect(() => {
     if (!socket) return;
 
-    socket.on("move", (move) => {
-      game.move(move);
-      setPosition(game.fen())
-    });
-
-    socket.on("start_game", ({ players }) => {
-      setPlayers(players);
+    socket.on(START_GAME, (data) => {
+      setPlayers(data.players);
       setIsGameStarted(true);
+      setIsGameOver(false);
+      setGame(new Chess());
+      toast.success("Game started!");
     });
 
-    socket.on("error", (message) => {
-      game.undo();
-      setPosition(game.fen())
-      toast.error(message);
+    socket.on(MOVE, (move) => {
+      game.move(move);
+      setGame(new Chess(game.fen()));
+    });
+
+    // Lắng nghe sự kiện kết thúc game
+    socket.on(GAME_OVER, (result) => {
+      setGameResult(result);
+      setIsGameStarted(false);
+      setIsGameOver(true);
+      toast.success(`Game Kết Thúc: ${result}`);
     });
 
     return () => {
-      socket.off("move");
+      socket.off(MOVE);
+      socket.off("game_over");
       socket.off("start_game");
-      socket.off("error");
     };
   }, [socket, game]);
 
   const createRoom = () => {
-    socket.emit("create_room", (roomId) => {
-      setRoomId(roomId);
+    socket.emit(CREATE_GAME, (game_id) => {
+      setRoomId(game_id);
     });
   };
 
   const joinRoom = () => {
     if (roomId) {
-      socket.emit("join_room", roomId);
+      socket.emit(JOIN_GAME, roomId);
     }
   };
 
-  const onDrop = (source, target) => {
-    const move =  { from: source, to: target, promotion: 'q' };
-
-    const result = game.move(move)
-
-    if (result) {
-      setPosition(game.fen())
-      socket.emit("move", { room_id: roomId, move });
-    } else {
-      game.undo()
+  const leaveGame = () => {
+    if (roomId) {
+      socket.emit(LEAVE_GAME, roomId);
     }
-
-    // setGame(new Chess(game.fen()));
-    // socket.emit("move", { room_id: roomId, move });
-    // return true;
   };
+
+  const onDrop = useCallback(
+    (source, target) => {
+      if (isGameOver) {
+        toast.error("Game đã kết thúc. Bạn không thể di chuyển quân cờ nữa.");
+        return false;
+      }
+
+      const move = { from: source, to: target, promotion: "q" };
+
+      socket.emit(MOVE, { game_id: roomId, move }, (response) => {
+        if (response.success) {
+          game.move(move);
+          setGame(new Chess(game.fen()));
+        } else {
+          toast.error(response.message || "Nước đi không hợp lệ.");
+        }
+      });
+
+      return true;
+    },
+    [socket, roomId, game, isGameOver]
+  );
+
+
+  const gameFen = game.fen();
+
+  const chessboardMemo = useMemo(
+    () => (
+      <Chessboard
+        position={gameFen}
+        onPieceDrop={onDrop}
+        boardWidth={600}
+        customBoardStyle={{
+          borderRadius: "8px",
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.4)",
+        }}
+        customSquareStyles={{
+          light: { backgroundColor: "#f0d9b5" },
+          dark: { backgroundColor: "#b58863" },
+        }}
+        arePiecesDraggable={!isGameOver} // Disable dragging if game is over
+      />
+    ),
+    [gameFen, onDrop, isGameOver]
+  );
 
   return (
     <div className="App">
-      {!isGameStarted ? (
-        <div>
-          <button onClick={createRoom}>Create Room</button>
-          <input
-            type="text"
-            value={roomId}
-            onChange={(e) => setRoomId(e.target.value)}
-            placeholder="Enter Room ID"
-          />
-          <button onClick={joinRoom}>Join Room</button>
+      {!isGameStarted && !isGameOver ? (
+        <div className="flex flex-col items-center justify-center h-full">
+          <button
+            onClick={createRoom}
+            className="bg-blue-500 text-white px-4 py-2 rounded m-2"
+          >
+            Create Room
+          </button>
+          <div className="flex items-center">
+            <input
+              type="text"
+              value={roomId}
+              onChange={(e) => setRoomId(e.target.value)}
+              placeholder="Enter Room ID"
+              className="border p-2 rounded mr-2"
+            />
+            <button
+              onClick={joinRoom}
+              className="bg-green-500 text-white px-4 py-2 rounded"
+            >
+              Join Room
+            </button>
+          </div>
         </div>
       ) : (
-        <div className="flex justify-center">
-          <div className="grid grid-cols-1 gap4 md:grid-cols-3">
-            <div>
-              <p>Room ID: {roomId}</p>
+        <div className="flex flex-row justify-center items-center gap-4">
+          <div> {chessboardMemo} </div>
+          {gameResult && (
+            <div className="mt-4 text-xl">
+              {isGameOver ? `Kết thúc: ${gameResult}` : ""}
             </div>
+          )}
+          {!isGameOver && (
             <div>
-              <Chessboard position={position} onPieceDrop={onDrop} />
+              <button
+                onClick={leaveGame}
+                className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+              >
+                LEAVE GAME
+              </button>
             </div>
-          </div>
+          )}
         </div>
       )}
     </div>
