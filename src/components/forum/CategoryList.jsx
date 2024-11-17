d633dcb29de1549008e139f4cfbf2f180d168992
Maou <nguyenvietquang.1601@gmail.com>
2024-11-17 22:38:44 +0700

    update forum UI

--- src/components/forum/CategoryList.jsx
+++ src/components/forum/CategoryList.jsx
@@ -1,24 +1,21 @@
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { MdOutlineForum } from "react-icons/md";
+import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 import api from "../../utils/axios";
 
+import { MdOutlineForum } from "react-icons/md";
+
 export const CategoryList = () => {
   const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        setLoading(true);
         const response = await api.get("/forum");
         const data = await response.data;
         setCategories(data.forum);
       } catch (error) {
         toast.error(error.response.data.message || "Something went wrong");
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -26,38 +23,34 @@ export const CategoryList = () => {
   }, []);
 
   return (
-    <div className="h-screen flex flex-col items-center">
-      <div className="border rounded-md bg-white w-7/8 m-4 shadow-md">
-        <div className="flex flex-row justify-between items-center m-7 py-4 min-h-[80px]">
-          <div className="flex flex-row items-center gap-4">
-            <MdOutlineForum className="size-24 text-gray-700 ml-4" />
-            <p className="text-4xl font-sans font-base text-[#4D4D4D]">
-              AutoChess Forum
-            </p>
-          </div>
+    <div className="flex flex-col items-center overflow-auto">
+      <div className="border border-gray-300 rounded-lg bg-white w-full max-w-screen-xl lg:w-[70%] h-full m-2 mb-4 shadow-lg">
+        {/* HEADER */}
+        <div className="flex flex-row items-center gap-4 m-7 ml-10 py-4">
+          {/* FORUM ICON */}
+          <MdOutlineForum className="size-20 text-gray-800" />
+          <p className="text-5xl font-base text-gray-800">Autochess Forum</p>
         </div>
+
+        {/* CONTENT */}
         <table className="w-full">
           <thead>
-            <tr className="bg-[#EDEBE9] border-b border-gray-300 font-sans">
+            <tr className="bg-[#E9E9E9] border-y border-gray-300">
               <td></td>
               <td>
-                <p className="text-gray-500 text-lg text-right mr-4">Topic</p>
+                <p className="text-gray-800 text-lg text-right mr-4">Topics</p>
               </td>
               <td>
-                <p className="text-gray-500 text-lg text-left">Posts</p>
+                <p className="text-gray-800 text-lg text-left">Posts</p>
               </td>
-              {/* <td>
-                <p className="text-gray-500 text-lg">Last Post</p>
-              </td> */}
             </tr>
           </thead>
           <tbody>
-            {categories.map((category, index) => (
+            {categories.map((category) => (
               <tr
                 key={category.category_id}
                 className={
-                  "hover:bg-gray-100 transition-colors h-16" +
-                  (index % 2 !== 0 ? " bg-[#EDEBE9]" : "")
+                  "hover:bg-gray-200 transition-colors h-16 border-y border-gray-300"
                 }
               >
                 <Link
@@ -65,19 +58,17 @@ export const CategoryList = () => {
                   state={category.category_name}
                 >
                   <td className="flex flex-col items-start gap-1 justify-center mx-10 my-5">
-                    {" "}
-                    {/* Tăng khoảng cách bên trái */}
-                    <p className="text-3xl text-blue-500">
+                    <p className="text-3xl text-emerald-600">
                       {category.category_name}
                     </p>
                     <p>{category.category_description}</p>
                   </td>
                 </Link>
                 <td className="text-right">
-                  <p className="mr-4">{category.post_count}</p>
+                  <p className="mr-4">{category.topic_count}</p>
                 </td>
                 <td className="text-left">
-                  <p>{category.topic_count}</p>
+                  <p>{category.post_count}</p>
                 </td>
               </tr>
             ))}
